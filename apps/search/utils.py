dfdf36b4c343f910ed24be2235a32977a5f6b512
Alex Monastyretsky <alex.mon1989@gmail.com>
2018-12-07 19:08:35 +0200

    рефакторинг представления расширенного поиска.

---
+++ apps/search/utils.py
@@ -0,0 +1,61 @@
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search, Q
+from .models import ObjType, InidCodeSchedule
+
+
+def get_search_groups(search_data):
+    """Разбивка поисковых данных на поисковые группы"""
+    search_groups = []
+    for obj_type in ObjType.objects.all():
+        # Поисковые запросы на заявки
+        search_groups.append({
+            'obj_type': obj_type,
+            'obj_state': 1,
+            'search_params': list(filter(
+                lambda x: x['obj_type'] == obj_type.pk and '1' in x['obj_state'],
+                search_data
+            ))
+        })
+        # Поисковые запросы на охранные документы
+        search_groups.append({
+            'obj_type': obj_type,
+            'obj_state': 2,
+            'search_params': list(filter(
+                lambda x: x['obj_type'] == obj_type.pk and '2' in x['obj_state'],
+                search_data
+            ))
+        })
+    # Фильтрация пустых групп
+    search_groups = filter(lambda x: len(x['search_params']) > 0, search_groups)
+    return list(search_groups)
+
+
+def elastic_search_groups(search_groups):
+    """Поиск в ElasticSearch по группам."""
+    all_hits = []
+    client = Elasticsearch()
+    for group in search_groups:
+        if group['search_params']:
+            qs = Q('query_string', query=f"{group['obj_type'].pk}", default_field='Document.idObjType')
+            qs &= Q('query_string', query=f"{group['obj_state']}", default_field='Document.Status')
+
+            for search_param in group['search_params']:
+                # Поле поиска ElasticSearch
+                inid_schedule = InidCodeSchedule.objects.filter(
+                    ipc_code__id=search_param['ipc_code'],
+                    schedule_type__obj_type=group['obj_type']
+                ).first()
+
+                # Проверка доступно ли поле для поиска
+                if inid_schedule.enable_search and inid_schedule.elastic_index_field is not None:
+                    qs &= Q(
+                        'query_string',
+                        query=f"{search_param['value']}",
+                        default_field=inid_schedule.elastic_index_field.field_name
+                    )
+            s = Search(using=client, index="uma").query(qs)
+            group['response'] = s.execute()
+            # Объединение результатов поиска
+            for hit in group['response']:
+                all_hits.append(hit)
+    return all_hits
--- apps/search/views.py
+++ apps/search/views.py
@@ -3,10 +3,9 @@ from django.db.models import F
 from django.forms import formset_factory, ValidationError
 from django.core.paginator import Paginator
 from django.http import Http404
-from .models import ObjType, InidCodeSchedule, SimpleSearchField, IpcCode, ElasticIndexField
+from .models import ObjType, InidCodeSchedule, SimpleSearchField
 from .forms import AdvancedSearchForm, SimpleSearchForm
-from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search, Q
+from .utils import get_search_groups, elastic_search_groups
 from operator import attrgetter
 
 
@@ -52,7 +51,8 @@ class AdvancedListView(TemplateView):
 
         # Типы ОПС
         context['obj_types'] = list(
-            ObjType.objects.order_by('id').annotate(value=F(f"obj_type_{context['lang_code']}")).values('id', 'value'))
+            ObjType.objects.order_by('id').annotate(value=F(f"obj_type_{context['lang_code']}")).values('id', 'value')
+        )
 
         # ИНИД-коды вместе с их реестрами
         context['ipc_codes'] = InidCodeSchedule.get_ipc_codes_with_schedules(context['lang_code'])
@@ -68,87 +68,33 @@ class AdvancedListView(TemplateView):
             # Валидация поисковой формы
             try:
                 is_valid = formset.is_valid()
-
-                # Признак того что производится поиск
-                context['is_search'] = True
             except ValidationError:
                 raise Http404("Некоректний пошуковий запит.")
+
+            # Признак того что производится поиск
+            context['is_search'] = True
 
             # Поиск в ElasticSearch
             if is_valid:
                 # Разбивка поисковых данных на поисковые группы
-                search_groups = []
-                for obj_type in ObjType.objects.all():
-                    # Поисковые запросы на заявки
-                    search_groups.append({
-                        'obj_type': obj_type,
-                        'obj_state': 1,
-                        'search_params': list(filter(
-                            lambda x: x['obj_type'] == obj_type.pk and '1' in x['obj_state'],
-                            formset.cleaned_data
-                        ))
-                    })
-                    # Поисковые запросы на охранные документы
-                    search_groups.append({
-                        'obj_type': obj_type,
-                        'obj_state': 2,
-                        'search_params': list(filter(
-                            lambda x: x['obj_type'] == obj_type.pk and '2' in x['obj_state'],
-                            formset.cleaned_data
-                        ))
-                    })
+                search_groups = get_search_groups(formset.cleaned_data)
 
                 # Поиск по каждой группе
-                client = Elasticsearch()
-                all_hits = []
-                for group in search_groups:
-                    if group['search_params']:
-                        qs = Q('query_string', query=f"{group['obj_type'].pk}", default_field='Document.idObjType')
-                        qs &= Q('query_string', query=f"{group['obj_state']}", default_field='Document.Status')
-
-                        for search_param in group['search_params']:
-                            # Поле поиска ElasticSearch
-                            inid_schedule = InidCodeSchedule.objects.filter(
-                                ipc_code__id=search_param['ipc_code'],
-                                schedule_type__obj_type=group['obj_type']
-                            ).first()
-
-                            # Проверка доступно ли поле для поиска
-                            if inid_schedule.enable_search and inid_schedule.elastic_index_field is not None:
-                                qs &= Q(
-                                    'query_string',
-                                    query=f"{search_param['value']}",
-                                    default_field=inid_schedule.elastic_index_field.field_name
-                                )
-
-                        s = Search(using=client, index="uma").query(qs)
-                        group['response'] = s.execute()
-                        # Объединение результатов поиска
-                        for hit in group['response']:
-                            all_hits.append(hit)
+                all_hits = elastic_search_groups(search_groups)
 
                 # Сортировка результатов поиска (обратная сортировка по релевантности)
                 all_hits = sorted(all_hits, key=attrgetter('meta.score'), reverse=True)
 
                 # Типы объектов в найденных результатах
-                context['res_obj_types'] = []
-                for obj_type in ObjType.objects.annotate(
-                        title=F(f"obj_type_{context['lang_code']}")
-                ).values('id', 'title'):
-                    count = len(list(filter(
-                        lambda x: x['Document']['idObjType'] == obj_type['id'],
-                        all_hits
-                    )))
-                    if count:
-                        context['res_obj_types'].append({'obj_type': obj_type})
+                obj_types = [{'id': x['obj_type'].id,
+                              'title': getattr(x['obj_type'], f"obj_type_{context['lang_code']}")}
+                             for x in search_groups]
+                context['res_obj_types'] = list({v['id']: v for v in obj_types}.values())
 
                 # Статусы в найденных результатах
                 context['res_obj_states'] = []
                 for i in range(1, 3):
-                    count = len(list(filter(
-                        lambda x: x['Document']['Status'] == i,
-                        all_hits
-                    )))
+                    count = len(list(filter(lambda x: x['Document']['Status'] == i, all_hits)))
                     if count:
                         context['res_obj_states'].append({'obj_state': i})
 
@@ -156,13 +102,12 @@ class AdvancedListView(TemplateView):
 
                 # Пагинатор
                 paginator = Paginator(all_hits, 10)
-                page = self.request.GET.get('page')
-                context['results'] = paginator.get_page(page)
+                context['results'] = paginator.get_page(self.request.GET.get('page'))
 
                 # Количество объектов определённых типов в отфильтрованных результатах
                 for obj_type in context['res_obj_types']:
                     obj_type['count'] = len(list(filter(
-                        lambda x: x['Document']['idObjType'] == obj_type['obj_type']['id'],
+                        lambda x: x['Document']['idObjType'] == obj_type['id'],
                         all_hits
                     )))
 
