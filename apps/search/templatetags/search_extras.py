b103cc7edcea11fd5582b6d03dc5e7a32c8c4bd1
Alex Monastyretsky <alex.mon1989@gmail.com>
2018-12-07 12:53:59 +0200

    вывод топографий в результатах поиска.

--- apps/search/templatetags/search_extras.py
+++ apps/search/templatetags/search_extras.py
@@ -15,11 +15,11 @@ def get_person_country(value):
     return values[1]
 
 
-@register.inclusion_tag('search/advanced/_partials/inv_um_claim_item.html')
-def inv_um_claim_item(hit, item_num):
+@register.inclusion_tag('search/advanced/_partials/inv_um_item.html')
+def inv_um_item(hit, item_num):
     return {'hit': hit, 'item_num': item_num}
 
 
-@register.inclusion_tag('search/advanced/_partials/inv_um_protective_item.html')
-def inv_um_protective_item(hit, item_num):
+@register.inclusion_tag('search/advanced/_partials/ld_item.html')
+def ld_item(hit, item_num):
     return {'hit': hit, 'item_num': item_num}
