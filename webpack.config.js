704839b0b78cd963de2ff84b44feaba367968637
Alexandr Monastyrtesky <alex.mon1989@gmail.com>
2019-01-08 13:05:10 +0200

    оптимизация построения стилей и скриптов.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,50 @@
 const path = require('path');
 const webpack = require('webpack');
-const BundleTracker = require('webpack-bundle-tracker');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
     mode: 'development',
     context: __dirname,
-    entry: './assets/js/index',
+    entry: ['./assets/js/index'],
     output: {
         path: path.resolve('./assets/bundles/'),
         filename: 'app.js'
     },
 
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: false,
+                extractComments: 'all',
+                uglifyOptions: {
+                    compress: true,
+                    output: null
+                }
+            }),
+            new OptimizeCSSAssetsPlugin({
+                cssProcessor: require("cssnano"),
+                cssProcessorPluginOptions: {
+                    preset: ["default", {discardComments: {removeAll: true}}]
+                }
+            })
+        ]
+    },
+
     plugins: [
-        new BundleTracker({filename: './webpack-stats.json'}),
         new VueLoaderPlugin(),
         new webpack.ProvidePlugin({
             jQuery: 'jquery',
             '$': 'jquery',
             'window.jQuery': 'jquery',
         }),
+        new MiniCssExtractPlugin({
+            filename: "styles.css"
+        })
     ],
 
     module: {
@@ -33,20 +58,19 @@ module.exports = {
                 loader: 'vue-loader'
             },
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.scss$/,
+                test: /\.(sa|sc|c)ss$/,
                 use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
                     {
-                        loader: "style-loader" // creates style nodes from JS strings
+                        loader: "css-loader",
+                        options: {
+                            minimize: true,
+                            sourceMap: true
+                        }
                     },
                     {
-                        loader: "css-loader" // translates CSS into CommonJS
-                    },
-                    {
-                        loader: "sass-loader" // compiles Sass to CSS
+                        loader: "sass-loader"
                     }
                 ]
             },
@@ -63,5 +87,4 @@ module.exports = {
         modules: ['node_modules'],
         alias: {vue: 'vue/dist/vue.js'}
     },
-
 };
