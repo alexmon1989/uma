a0b4969258e91b67c38970c05eb6e8eb7241e548
Alexandr Monastyrtesky <alex.mon1989@gmail.com>
2019-01-10 11:54:04 +0200

    сохранение параметров запроса при смене языка с помощью меню выбора языка.

--- templatetags/uma_extras.py
+++ templatetags/uma_extras.py
@@ -7,7 +7,7 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def change_language(context, lang=None, *args, **kwargs):
-    path = context['request'].path
+    path = context['request'].get_full_path()
     return translate_url(path, lang)
 
 
