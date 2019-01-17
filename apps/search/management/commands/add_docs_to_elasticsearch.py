c30e66dbfb03371ac026b2b31b746b5d58cda6de
Alexandr Monastyrtesky <alex.mon1989@gmail.com>
2019-01-17 14:26:43 +0200

    доработки индексатора.

--- apps/search/management/commands/add_docs_to_elasticsearch.py
+++ apps/search/management/commands/add_docs_to_elasticsearch.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from elasticsearch import Elasticsearch, exceptions as elasticsearch_exceptions
 from apps.search.models import IpcAppList
 import json
-from uma.utils import chardet_open
 import os.path
 
 
@@ -15,7 +14,7 @@ class Command(BaseCommand):
         es = Elasticsearch()
 
         # Получение документов для индексации
-        documents = IpcAppList.objects.filter(elasticindexed=0).values(
+        documents = IpcAppList.objects.filter(elasticindexed=0, obj_type__id__in=(1, 2, 3)).values(
             'id',
             'files_path',
             'obj_type_id',
@@ -41,89 +40,103 @@ class Command(BaseCommand):
                     json_path = os.path.join(doc_files_path, f"{file_name}-К.json")
 
                 res = {}
+                data = None
+
+                # Чтение содержимого JSON в data
                 try:
-                    f = chardet_open(json_path, 'r')
+                    f = open(json_path, 'r', encoding='utf16')
                     try:
-                        # Чтение содержимого JSON
                         data = json.loads(f.read())
-
-                        # Секция Document
-                        res['Document'] = data.get('Document')
-
-                        # Бибилиография
-                        biblio_data = None
-                        if data.get('Patent'):
-                            biblio_data = data['Patent']
-                            res['Patent'] = biblio_data
-                        elif data.get('Claim'):
-                            biblio_data = data['Claim']
-                            res['Claim'] = biblio_data
-
-                        if biblio_data is None:
-                            self.stdout.write(
-                                self.style.ERROR(f"Error: biblio data missed in file: {json_path}"))
-                        else:
-                            # Обработка I_72 для избежания ошибки добавления в индекс ElasticSearch
-                            for I_72 in biblio_data.get('I_72', []):
-                                if I_72.get('I_72.N'):
-                                    I_72['I_72.N.E'] = I_72.pop('I_72.N')
-                                if I_72.get('I_72.C'):
-                                    I_72['I_72.C.E'] = I_72.pop('I_72.C')
-
-                            # Обработка I_73 для избежания ошибки добавления в индекс ElasticSearch
-                            for I_72 in biblio_data.get('I_73', []):
-                                if I_72.get('I_73.N.U'):
-                                    I_72['I_73.N'] = I_72.pop('I_73.N.U')
-                                if I_72.get('I_73.N.R'):
-                                    I_72['I_73.N'] = I_72.pop('I_73.N.R')
-                                if I_72.get('I_73.N.E'):
-                                    I_72['I_73.N'] = I_72.pop('I_73.N.E')
-                                if I_72.get('I_73.C.U'):
-                                    I_72['I_73.C'] = I_72.pop('I_73.C.U')
-                                if I_72.get('I_73.C.R'):
-                                    I_72['I_73.C'] = I_72.pop('I_73.C.R')
-                                if I_72.get('I_73.C.E'):
-                                    I_72['I_73.C'] = I_72.pop('I_73.C.E')
-
-                            # Состояние делопроизводства
-                            if data.get('DOCFLOW'):
-                                res['DOCFLOW'] = data['DOCFLOW']
-
-                            # Оповещения
-                            if data.get('TRANSACTIONS'):
-                                res['TRANSACTIONS'] = data['TRANSACTIONS']
-
-                            # Поисковые данные (для сортировки и т.д.)
-                            res['search_data'] = {
-                                'obj_state': 2 if doc['registration_number'] else 1,
-                                'app_number': biblio_data.get('I_21'),
-                                'app_date': biblio_data.get('I_22'),
-                                'protective_doc_number': biblio_data.get('I_11'),
-                                'rights_date': biblio_data.get('I_24'),
-                                'applicant': [list(x.values())[0] for x in biblio_data.get('I_71', [])],
-                                'inventor': [list(x.values())[0] for x in biblio_data.get('I_72', [])],
-                                'owner': [list(x.values())[0] for x in biblio_data.get('I_73', [])],
-                                'agent': biblio_data.get('I_74'),
-                                'title': [list(x.values())[0] for x in biblio_data.get('I_54', [])],
-                            }
-
-                            # Запись в индекс
-                            try:
-                                es.index(index=settings.ELASTIC_INDEX_NAME, doc_type='_doc', id=doc['id'], body=res)
-                            except elasticsearch_exceptions.RequestError as e:
-                                self.stdout.write(self.style.ERROR(f"ElasticSearch RequestError: {e}: {json_path}"))
-                            else:
-                                # Пометка в БД что этот документ проиндексирован
-                                IpcAppList.objects.filter(id=doc['id']).update(elasticindexed=1)
-
                     except json.decoder.JSONDecodeError as e:
                         self.stdout.write(self.style.ERROR(f"JSONDecodeError: {e}: {json_path}"))
-
+                except (UnicodeDecodeError, UnicodeError):
+                    f = open(json_path, 'r', encoding='utf-8')
+                    try:
+                        data = json.loads(f.read())
+                    except json.decoder.JSONDecodeError as e:
+                        self.stdout.write(self.style.ERROR(f"JSONDecodeError: {e}: {json_path}"))
                 except FileNotFoundError as e:
                     self.stdout.write(self.style.ERROR(f"FileNotFoundError: {e}"))
-                except UnicodeDecodeError as e:
-                    self.stdout.write(self.style.ERROR(f"UnicodeDecodeError: {e}: {json_path}"))
-                except UnicodeError as e:
-                    self.stdout.write(self.style.ERROR(f"UnicodeError: {e}: {json_path}"))
+
+                if data is not None:
+                    # Секция Document
+                    res['Document'] = data.get('Document')
+
+                    # Бибилиография
+                    biblio_data = None
+                    if data.get('Patent'):
+                        biblio_data = data['Patent']
+                        res['Patent'] = biblio_data
+                    elif data.get('Claim'):
+                        biblio_data = data['Claim']
+                        res['Claim'] = biblio_data
+
+                    if biblio_data is None:
+                        self.stdout.write(
+                            self.style.ERROR(f"Error: biblio data missed in file: {json_path}"))
+                    else:
+                        # Обработка I_71 для избежания ошибки добавления в индекс ElasticSearch
+                        i_71 = biblio_data.get('I_71', [])
+                        if type(i_71) is dict:
+                            i_71 = biblio_data['I_71']['I_71.N']
+                        biblio_data['I_71'] = i_71
+
+                        # Обработка I_72 для избежания ошибки добавления в индекс ElasticSearch
+                        for I_72 in biblio_data.get('I_72', []):
+                            if I_72.get('I_72.N'):
+                                I_72['I_72.N.E'] = I_72.pop('I_72.N')
+                            if I_72.get('I_72.C'):
+                                I_72['I_72.C.E'] = I_72.pop('I_72.C')
+
+                        # Обработка I_73 для избежания ошибки добавления в индекс ElasticSearch
+                        i_73 = biblio_data.get('I_73', [])
+                        if type(i_73) is dict:
+                            i_73 = biblio_data['I_73']['I_73.N']
+
+                        for item_i_73 in i_73:
+                            if item_i_73.get('I_73.N.U'):
+                                item_i_73['I_73.N'] = item_i_73.pop('I_73.N.U')
+                            if item_i_73.get('I_73.N.R'):
+                                item_i_73['I_73.N'] = item_i_73.pop('I_73.N.R')
+                            if item_i_73.get('I_73.N.E'):
+                                item_i_73['I_73.N'] = item_i_73.pop('I_73.N.E')
+                            if item_i_73.get('I_73.C.U'):
+                                item_i_73['I_73.C'] = item_i_73.pop('I_73.C.U')
+                            if item_i_73.get('I_73.C.R'):
+                                item_i_73['I_73.C'] = item_i_73.pop('I_73.C.R')
+                            if item_i_73.get('I_73.C.E'):
+                                item_i_73['I_73.C'] = item_i_73.pop('I_73.C.E')
+                        biblio_data['I_73'] = i_73
+
+                        # Состояние делопроизводства
+                        if data.get('DOCFLOW'):
+                            res['DOCFLOW'] = data['DOCFLOW']
+
+                        # Оповещения
+                        if data.get('TRANSACTIONS'):
+                            res['TRANSACTIONS'] = data['TRANSACTIONS']
+
+                        # Поисковые данные (для сортировки и т.д.)
+                        res['search_data'] = {
+                            'obj_state': 2 if doc['registration_number'] else 1,
+                            'app_number': biblio_data.get('I_21'),
+                            'app_date': biblio_data.get('I_22'),
+                            'protective_doc_number': biblio_data.get('I_11'),
+                            'rights_date': biblio_data.get('I_24'),
+                            'applicant': [list(x.values())[0] for x in biblio_data.get('I_71', [])],
+                            'inventor': [list(x.values())[0] for x in biblio_data.get('I_72', [])],
+                            'owner': [list(x.values())[0] for x in biblio_data.get('I_73', [])],
+                            'agent': biblio_data.get('I_74'),
+                            'title': [list(x.values())[0] for x in biblio_data.get('I_54', [])],
+                        }
+
+                        # Запись в индекс
+                        try:
+                            es.index(index=settings.ELASTIC_INDEX_NAME, doc_type='_doc', id=doc['id'], body=res)
+                        except elasticsearch_exceptions.RequestError as e:
+                            self.stdout.write(self.style.ERROR(f"ElasticSearch RequestError: {e}: {json_path}"))
+                        else:
+                            # Пометка в БД что этот документ проиндексирован
+                            IpcAppList.objects.filter(id=doc['id']).update(elasticindexed=1)
 
         self.stdout.write(self.style.SUCCESS('Finished'))
