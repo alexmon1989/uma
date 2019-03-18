214839f84b2e5f35c209276ef74a2cd9921e1beb
Alexandr Monastyrtesky <alex.mon1989@gmail.com>
2019-03-18 11:40:17 +0200

    перевод страницы Помощь на английский

--- apps/help/models.py
+++ apps/help/models.py
@@ -5,11 +5,13 @@ from ckeditor_uploader.fields import RichTextUploadingField
 
 class Help(TimeStampedModel):
     """Модель для страницы 'Про сервіс'"""
-    title = models.CharField('Заголовок', max_length=255, default='Допомога')
-    content = RichTextUploadingField('Зміст', blank=True)
+    title_uk = models.CharField('Заголовок', max_length=255, default='Допомога')
+    content_uk = RichTextUploadingField('Зміст', blank=True)
+    title_en = models.CharField('Заголовок', max_length=255, default='Help')
+    content_en = RichTextUploadingField('Зміст', blank=True)
 
     def __str__(self):
-        return self.title
+        return self.title_uk
 
     class Meta:
         verbose_name = 'Зміст сторінки'
--- apps/help/views.py
+++ apps/help/views.py
@@ -5,4 +5,12 @@ from .models import Help
 def help_page(request):
     """Отображает страницу Помощь"""
     data, created = Help.objects.get_or_create()
-    return render(request, 'help/help.html', {'data': data})
+    lang = request.LANGUAGE_CODE
+    return render(
+        request,
+        'help/help.html',
+        {
+            'title': getattr(data, f"title_{lang}"),
+            'content': getattr(data, f"content_{lang}")
+        }
+    )
