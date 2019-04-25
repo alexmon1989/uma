55745b69bb4a0ba37136c8af4197c65896222d29
Alexandr Monastyrtesky <alex.mon1989@gmail.com>
2019-04-25 15:00:58 +0300

    fix change url parameter error.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,21 +1,46 @@
 /**
  * Обновляет GET-параметр в url.
  */
-function updateURLParameter(url, param, paramVal) {
+function updateURLParameter(url, param, paramVal)
+{
+    let TheAnchor = null;
     let newAdditionalURL = "";
     let tempArray = url.split("?");
     let baseURL = tempArray[0];
     let additionalURL = tempArray[1];
     let temp = "";
-    if (additionalURL) {
+
+    if (additionalURL)
+    {
+        let tmpAnchor = additionalURL.split("#");
+        let TheParams = tmpAnchor[0];
+            TheAnchor = tmpAnchor[1];
+        if(TheAnchor)
+            additionalURL = TheParams;
+
         tempArray = additionalURL.split("&");
-        for (let i = 0; i < tempArray.length; i++) {
-            if (tempArray[i].split('=')[0] !== param) {
+
+        for (let i=0; i<tempArray.length; i++)
+        {
+            if(tempArray[i].split('=')[0] != param)
+            {
                 newAdditionalURL += temp + tempArray[i];
                 temp = "&";
             }
         }
     }
+    else
+    {
+        let tmpAnchor = baseURL.split("#");
+        let TheParams = tmpAnchor[0];
+            TheAnchor  = tmpAnchor[1];
+
+        if(TheParams)
+            baseURL = TheParams;
+    }
+
+    if(TheAnchor)
+        paramVal += "#" + TheAnchor;
 
     let rows_txt = temp + "" + param + "=" + paramVal;
     return baseURL + "?" + newAdditionalURL + rows_txt;
