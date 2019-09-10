b2d83b67a40b4a0668a42d1cad975b39f63c0a61
Alexandr Monastyrtesky <alex.mon1989@gmail.com>
2019-09-10 11:37:47 +0300

    sending all email through celery

--- apps/contacts/tasks.py
+++
@@ -1,8 +0,0 @@
-from celery import shared_task
-from django.core.mail import send_mail
-
-
-@shared_task
-def send_contact_form(data):
-    """Задача для отправки письма с контактной формы."""
-    send_mail(**data)
--- uma/celery.py
+++ uma/celery.py
@@ -1,5 +1,8 @@
 import os
+import socket
 from celery import Celery
+from celery.signals import task_failure
+from django.core.mail import mail_admins
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uma.settings')
@@ -14,3 +17,30 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+
+@task_failure.connect()
+def celery_task_failure_email(**kwargs):
+    """ celery 4.0 onward has no method to send emails on failed tasks
+    so this event handler is intended to replace it
+    """
+    subject = u"[Django][{queue_name}@{host}] Error: Task {sender.name} ({task_id}): {exception}".format(
+        queue_name=u'celery',  # `sender.queue` doesn't exist in 4.1?
+        host=socket.gethostname(),
+        **kwargs
+    )
+
+    message = u"""Task {sender.name} with id {task_id} raised exception:
+{exception!r}
+
+
+Task was called with args: {args} kwargs: {kwargs}.
+
+The contents of the full traceback was:
+
+{einfo}
+    """.format(
+        **kwargs
+    )
+
+    mail_admins(subject, message)
