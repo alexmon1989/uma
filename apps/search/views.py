fceec491db6595be25199b269ce8295171dd2cd2
Alexandr Monastyrtesky <alex.mon1989@gmail.com>
2019-10-10 16:53:57 +0300

    editable text at search pages

--- apps/search/views.py
+++ apps/search/views.py
@@ -9,7 +9,8 @@ from django.shortcuts import redirect
 from django.views.decorators.http import require_POST
 from django.contrib.admin.views.decorators import user_passes_test
 from django.template.loader import render_to_string
-from .models import ObjType, InidCodeSchedule, SimpleSearchField, OrderService, OrderDocument, IpcAppList
+from .models import (ObjType, InidCodeSchedule, SimpleSearchField, OrderService, OrderDocument, IpcAppList,
+                     SimpleSearchPage, AdvancedSearchPage)
 from .forms import AdvancedSearchForm, SimpleSearchForm
 from .utils import (get_client_ip, paginate_results)
 from urllib.parse import parse_qs, urlparse
@@ -33,6 +34,10 @@ class SimpleListView(TemplateView):
         # Текущий язык приложения
         lang_code = 'ua' if self.request.LANGUAGE_CODE == 'uk' else 'en'
         context['lang_code'] = lang_code
+
+        # Данные страницы
+        page_data, created = SimpleSearchPage.objects.get_or_create()
+        context['page_description'] = getattr(page_data, f"description_{self.request.LANGUAGE_CODE}")
 
         # Параметры поиска
         context['search_parameter_types'] = list(SimpleSearchField.objects.annotate(
@@ -106,6 +111,10 @@ class AdvancedListView(TemplateView):
 
         # Текущий язык приложения
         context['lang_code'] = 'ua' if self.request.LANGUAGE_CODE == 'uk' else 'en'
+
+        # Данные страницы
+        page_data, created = AdvancedSearchPage.objects.get_or_create()
+        context['page_description'] = getattr(page_data, f"description_{self.request.LANGUAGE_CODE}")
 
         # Типы ОПС
         context['obj_types'] = list(
