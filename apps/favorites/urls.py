3ae34bcaecfa9d3751ae8cae47a56805c2f81f67
Alexandr Monastyrtesky <alex.mon1989@gmail.com>
2020-04-24 16:50:46 +0300

    clearing favorites list

--- apps/favorites/urls.py
+++ apps/favorites/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import IndexView, get_results_html
+from .views import IndexView, get_results_html, ClearRedirectView
 
 app_name = 'favorites'
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
     path('results/', get_results_html, name="get_results_html"),
+    path('clear/', ClearRedirectView.as_view(), name="clear"),
 ]
