128baf8e5f6114158bc4875ea306a4468fb386c0
Alexandr Monastyrtesky <alex.mon1989@gmail.com>
2020-06-15 12:51:44 +0300

    obj_state filling

--- apps/api/management/commands/fill_open_data_table.py
+++ apps/api/management/commands/fill_open_data_table.py
@@ -44,6 +44,7 @@ class Command(BaseCommand):
                 open_data_record.app_number = item.app_number
                 open_data_record.registration_number = item.registration_number
                 open_data_record.registration_date = make_aware(item.registration_date)
+                open_data_record.obj_state = 1 if item.registration_date is None else 2
 
                 # Получение данных с ElasticSearch
                 data = Search(using=self.es, index=settings.ELASTIC_INDEX_NAME).query("match", _id=item.id).execute()
