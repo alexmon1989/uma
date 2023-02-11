27ee65a2f0aee606ce0cef6625f617d303513707
Alexandr Monastyrtesky <alex.mon1989@gmail.com>
2023-02-11 21:10:25 +0200

    remove join table from api opendata

--- apps/api/models.py
+++ apps/api/models.py
@@ -21,34 +21,9 @@ class OpenData(models.Model):
     app_date = models.DateTimeField(blank=True, null=True, default=None, db_index=True)
     registration_number = models.CharField(blank=True, null=True, default=None, max_length=32)
     registration_date = models.DateTimeField(blank=True, null=True, default=None, db_index=True)
+    files_path = models.CharField(max_length=500, blank=True, null=True)
     is_visible = models.BooleanField(default=True)
 
     class Meta:
         verbose_name = 'Відкриті дані'
         verbose_name_plural = 'Відкриті дані'
-
-
-class OpenDataViewModel(models.Model):
-    """Модель для открытых данных, работающая через view."""
-    app = models.ForeignKey(IpcAppList, on_delete=models.CASCADE, verbose_name='Заявка')
-    data = models.TextField(verbose_name='Дані у форматі JSON')
-    data_docs = models.TextField(verbose_name='Стан діловодства у форматі JSON', default=None, null=True, blank=True)
-    data_payments = models.TextField(
-        verbose_name='Платежі, збори',
-        default=None,
-        null=True,
-        blank=True
-    )
-    # Поля для денормализации данных с целью ускорения поиска
-    obj_type = models.ForeignKey(ObjType, blank=True, null=True, on_delete=models.CASCADE, default=None)
-    obj_state = models.PositiveSmallIntegerField(default=2)
-    last_update = models.DateTimeField(blank=True, null=True, default=None, db_index=True)
-    app_number = models.CharField(max_length=100, blank=True, null=True, default=None)
-    app_date = models.DateTimeField(blank=True, null=True, default=None, db_index=True)
-    registration_number = models.CharField(blank=True, null=True, default=None, max_length=32)
-    registration_date = models.DateTimeField(blank=True, null=True, default=None, db_index=True)
-    is_visible = models.BooleanField(default=True)
-
-    class Meta:
-        db_table = 'opendata_api_view'
-        managed = False
