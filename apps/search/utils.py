2fc44bce8b4f1474a5e7c415a29c2d1fda3a553e
Olexandr Monastyretskyi <alex.mon1989@gmail.com>
2024-03-26 11:50:14 +0200

    fix mark_status_code getting

--- apps/search/utils.py
+++ apps/search/utils.py
@@ -1907,7 +1907,12 @@ def get_registration_status_color(hit):
 def get_fixed_mark_status_code(app_data):
     """Анализирует список документов и возвращает код статуса согласно их наличию."""
     result = int(app_data['Document'].get('MarkCurrentStatusCodeType', 0))
+
+    stages = app_data['TradeMark']['TrademarkDetails'].get('stages')
+
     for doc in app_data['TradeMark'].get('DocFlow', {}).get('Documents', []):
+        if stages and stages[4]['status'] == 'done' and result < 2000:
+            result = 3000
         if ('ТM-1.1' in doc['DocRecord']['DocType'] or 'ТМ-1.1' in doc['DocRecord']['DocType']) and result < 2000:
             result = 3000
         if ('Т-05' in doc['DocRecord']['DocType'] or 'Т-5' in doc['DocRecord']['DocType']) and result < 3000:
