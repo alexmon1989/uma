7db29b95cd301a4e5a250316f4aa0843a3d4e08a
Olexandr Monastyretskyi <alex.mon1989@gmail.com>
2025-06-04 16:07:38 +0300

    fix inv um stages

--- apps/search/templatetags/search_extras.py
+++ apps/search/templatetags/search_extras.py
@@ -481,7 +481,7 @@ def app_stages_inv_um(app):
 
     # Признаки того что делопроизводство возобновлено
     if is_stopped:
-        for x in ['[В21а]', '[В21б]', '[В21]', '[В22]']:
+        for x in ['[В21а]', '[В21б]', '[В21]', '[В22]', '[В14]']:  # [В14] - перетворення корисної моделі у патент
             for doc_type in doc_types:
                 if x in doc_type:
                     is_stopped = False
@@ -573,6 +573,7 @@ def app_stages_inv_um(app):
         stages[4]['status'] = 'not-used'
 
     # Определение текущей стадии или стадии, на которой было остановлено делопроизводство
+    print(stages)
     for i, s in enumerate(stages):
         if s['status'] == 'done':
             if i != 0:
